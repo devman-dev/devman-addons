a76857b360058116bc7a044842b0b8936eb54549
NelsonTontarelli <nelartz01@gmail.com>
2024-11-15 13:05:03 -0300

    [FIX][ADD] Corrigiendo errores y añadiendo campos en res.partner

--- payment_collection/models/res_partner.py
+++ payment_collection/models/res_partner.py
@@ -4,7 +4,14 @@ from odoo import fields, models, api
 class ResPartnerInherit(models.Model):
     _inherit = 'res.partner'
 
-    cbu_destination = fields.Char('CBU')
-    cvu_destination = fields.Char('CVU')
-    alias_destination = fields.Char('Alias')
-    name_account_destination = fields.Char('Nombre de Cuenta')
+    cbu_destination = fields.Char('CBU Destino')
+    cvu_destination = fields.Char('CVU Destino')
+    alias_destination = fields.Char('Alias Destino')
+    name_account_destination = fields.Char('Nombre Cuenta Destino')
+    cuit_destination = fields.Char('CUIT Destino')
+    
+    cbu_account_source = fields.Char('CBU Origen')
+    cvu_account_source = fields.Char('CVU Origen')
+    alias_account_source = fields.Char('Alias Origen')
+    cuit_account_source = fields.Char('CUIT Origen')
+    name_account_source = fields.Char('Nombre Cuenta Origen')
