7e3f9b34c29e2c17d403f745a672287a70e39e27
NelsonTontarelli <nelartz01@gmail.com>
2024-12-18 17:16:17 -0300

    [FIX] Billetera QWEB

--- billetera_pagoflex/controllers/transferencia.py
+++ billetera_pagoflex/controllers/transferencia.py
@@ -1,4 +1,6 @@
 from odoo.http import request, Controller, route
+import requests
+import json
 
 
 class WebFormWalletController(Controller):
@@ -8,15 +10,48 @@ class WebFormWalletController(Controller):
         available_balance = customer.customer_available_balance if customer else 0.00
         return request.render('billetera_pagoflex.web_form_template_wallet', {'available_balance': available_balance})
 
-
     @route('/wallet/transfer/accounts', auth='public', website=True, methods=['GET'])
     def web_form_transfer(self, **kwargs):
-        return request.render('billetera_pagoflex.web_form_template_transfer')
+        data = requests.get('http://localhost:8017/bank/get_data')
+        response = data.json()
+        return request.render('billetera_pagoflex.web_form_template_transfer', {'list_accounts': response})
 
     @route('/wallet/transfer/accounts/new_account', auth='public', website=True, methods=['GET'])
     def web_form_transfer_new_account(self, **kwargs):
         return request.render('billetera_pagoflex.web_form_template_transfer_new_account')
-    
+
     @route('/wallet/transfer/accounts/new_account/search', auth='public', website=True, methods=['GET'])
     def web_form_transfer_new_account_search(self, **kwargs):
         return request.render('billetera_pagoflex.web_form_template_transfer_new_account_search')
+
+    @route('/bank/get_data', type='http', auth='public', methods=['GET'])
+    def bank_get_data(self, **datos):
+        dict_data = [
+            {
+                'id': '1',
+                'cbu': '1234567890112345678901',
+                'cvu': '1234567890112345678901',
+                'alias': 'alias.demo',
+                'name_account': 'Datos Demostracion',
+            },
+            {
+                'id': '2',
+                'cbu': '1234567890112345678901',
+                'cvu': '1234567890112345678901',
+                'alias': 'alias.demo2',
+                'name_account': 'Datos Demostracion2',
+            },
+        ]
+
+        return request.make_response(json.dumps(dict_data), headers=[('Content-Type', 'application/json')])
+
+    @route('/wallet/transfer/account/<int:account_id>', auth='user', website=True)
+    def transfer_account(self, account_id, **kwargs):
+        account = {
+            'id': '2',
+            'cbu': '1234567890112345678901',
+            'cvu': '1234567890112345678901',
+            'alias': 'alias.demo2',
+            'name_account': 'Datos Demostracion2',
+        }
+        return request.render('billetera_pagoflex.transfer_account_template', {'account': account})
