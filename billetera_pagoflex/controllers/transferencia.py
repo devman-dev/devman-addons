8e2b42e35129c5ac34782711e468494797ec06b6
mmarziali <matiasmarziali96@gmail.com>
2024-12-19 18:01:40 -0300

    [ADD] añadimos billetera qweb

--- billetera_pagoflex/controllers/transferencia.py
+++ billetera_pagoflex/controllers/transferencia.py
@@ -10,17 +10,32 @@ class WebFormWalletController(Controller):
         available_balance = customer.customer_available_balance if customer else 0.00
         return request.render('billetera_pagoflex.web_form_template_wallet', {'available_balance': available_balance})
 
-    @route('/wallet/transfer/accounts', auth='public', website=True, methods=['GET'])
+    @route('/wallet/transfer/accounts', auth='user', website=True, methods=['GET'])
     def web_form_transfer(self, **kwargs):
         data = requests.get('http://localhost:8017/bank/get_data')
         response = data.json()
         return request.render('billetera_pagoflex.web_form_template_transfer', {'list_accounts': response})
 
-    @route('/wallet/transfer/accounts/new_account', auth='public', website=True, methods=['GET'])
+    @route('/wallet/transfer/accounts/new_account', auth='user', website=True, methods=['GET'])
     def web_form_transfer_new_account(self, **kwargs):
         return request.render('billetera_pagoflex.web_form_template_transfer_new_account')
 
-    @route('/wallet/transfer/accounts/new_account/search', auth='public', website=True, methods=['GET'])
+
+    @route('/wallet/transfer/accounts/confirm_account', auth='user', website=True, methods=['GET'])
+    def web_form_transfer_confirm_account(self, **kwargs):
+        account = {
+            'id': '2',
+            'cbu': '1234567890112345678901',
+            'cvu': '1234567890112345678901',
+            'alias': 'alias.demo2',
+            'name_account': 'Datos Demostracion2',
+            'cuit': '12345678901'
+        }
+        return request.render('billetera_pagoflex.web_form_template_transfer_confirm_account',{'account':account})
+
+
+
+    @route('/wallet/transfer/accounts/new_account/search', auth='user', website=True, methods=['GET'])
     def web_form_transfer_new_account_search(self, **kwargs):
         return request.render('billetera_pagoflex.web_form_template_transfer_new_account_search')
 
@@ -42,7 +57,6 @@ class WebFormWalletController(Controller):
                 'name_account': 'Datos Demostracion2',
             },
         ]
-
         return request.make_response(json.dumps(dict_data), headers=[('Content-Type', 'application/json')])
 
     @route('/wallet/transfer/account/<int:account_id>', auth='user', website=True)
@@ -55,3 +69,23 @@ class WebFormWalletController(Controller):
             'name_account': 'Datos Demostracion2',
         }
         return request.render('billetera_pagoflex.transfer_account_template', {'account': account})
+
+    @route('/wallet/transfer/account/revision/<int:account_id>', auth='user', website=True)
+    def revision_account(self, account_id, **kwargs):
+
+        account = {
+            'id': '2',
+            'cbu': '1234567890112345678901',
+            'cvu': '1234567890112345678901',
+            'alias': 'alias.demo2',
+            'name_account': 'Datos Demostracion2',
+            'cuit': '12345678901'
+        }
+        return request.render('billetera_pagoflex.web_form_template_transfer_account_revision', {'account': account,'amount': 100})
+
+
+    @route('/wallet/transfer/sended', auth='user', website=True)
+    def transfer_sended(self, **kwargs):
+        return request.render('billetera_pagoflex.web_form_template_transfer_sended')
+
+
