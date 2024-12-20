d55a13768e40c7d85a8577683817da3ce86f9526
mmarziali <matiasmarziali96@gmail.com>
2024-12-20 19:39:43 -0300

    [ADD] reporte movimimentos bancarios

--- payment_collection/report/__init__.py
+++ payment_collection/report/__init__.py
@@ -1,1 +1,2 @@
 from . import report_collection_transaction
+from . import  report_bank_movements_month
--- payment_collection/wizard/__init__.py
+++ payment_collection/wizard/__init__.py
@@ -2,3 +2,4 @@ from . import payment_wiz
 from . import report_agent_wiz
 from . import commi_trans_wiz
 from . import recalculate_button
+from . import bank_movements_month
