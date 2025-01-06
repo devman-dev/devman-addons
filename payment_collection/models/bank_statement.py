2458655d946e1a77a8f6b8206b5cffd3052a0581
NelsonTontarelli <nelartz01@gmail.com>
2025-01-06 14:29:36 -0300

    [FIX] payment_collection: Ocultamos campos en la vista tree [ADD] payment_collection: Boton de romper conciliación, boton para cargar las cuentas de destino, campo monto en los filtros de busqueda

--- payment_collection/models/bank_statement.py
+++ payment_collection/models/bank_statement.py
@@ -3,30 +3,39 @@ from odoo import models, fields, api
 
 class BankStatement(models.Model):
     _name = 'bank.statement'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Bank Statement'
     _rec_name = 'titular'
 
-    date = fields.Date(string='Fecha de Transacción', required=False)
-    amount = fields.Float(string='Monto', required=False)
-    titular = fields.Char(string='Titular', required=False)
-    cuit = fields.Char(string='CUIT', required=False)
-    bank = fields.Char(string='Banco Origen', required=False)
-    cvu = fields.Char(string='CVU', required=False)
-    cbu = fields.Char(string='CBU', required=False)
-    alias = fields.Char(string='Alias', required=False)
-    id_coelsa = fields.Char(string='Coelsa ID', required=False)
-    reference = fields.Char(string='Referencia', help='Referencia adicional para la declaración.')
-    is_concilied = fields.Boolean(string='Conciliado', defualt=False)
-    concilied_id = fields.Many2one('collection.transaction', string='Conciliado con')
-    destination_bank = fields.Char(string='Banco Destino', required=False)
+    date = fields.Date(string='Fecha de Transacción', required=False, tracking=True)
+    amount = fields.Float(string='Monto', required=False, tracking=True)
+    titular = fields.Char(string='Titular', required=False, tracking=True)
+    cuit = fields.Char(string='CUIT', required=False, tracking=True)
+    bank = fields.Char(string='Banco Origen', required=False, tracking=True)
+    cvu = fields.Char(string='CVU', required=False, tracking=True)
+    cbu = fields.Char(string='CBU', required=False, tracking=True)
+    alias = fields.Char(string='Alias', required=False, tracking=True)
+    id_coelsa = fields.Char(string='Coelsa ID', required=False, tracking=True)
+    reference = fields.Char(string='Referencia', help='Referencia adicional para la declaración.', tracking=True)
+    is_concilied = fields.Boolean(string='Conciliado', defualt=False, tracking=True)
+    concilied_id = fields.Many2one('collection.transaction', string='Conciliado con', tracking=True)
+    destination_bank = fields.Char(string='Banco Destino', required=False, tracking=True)
+
+    def break_conciliation(self):
+        for rec in self:
+            if rec.concilied_id:
+                rec.concilied_id.concilied_id = False
+                rec.concilied_id.is_concilied = False
+                rec.concilied_id = False
+                rec.is_concilied = False
 
     def open_wiz(self):
-        condi = [('date', '=', self.date),('is_commission', '=', False), ('is_concilied', '=', False),('amount','=',self.amount)]
+        condi = [('date', '=', self.date), ('is_commission', '=', False), ('is_concilied', '=', False), ('amount', '=', self.amount)]
         if not self.env.context.get('without_titular', False):
             condi.append(('customer.name', 'ilike', f'%{self.titular}%'))
-            
+
         records = self.env['collection.transaction'].sudo().search(condi)
-        
+
         return {
             'name': 'Conciliación de Transacciones',
             'type': 'ir.actions.act_window',
@@ -39,9 +48,8 @@ class BankStatement(models.Model):
                 'date': self.date,
                 'amount': self.amount,
                 'titular': self.titular,
-                'destination_bank':self.destination_bank,
-                'bank':self.bank,
-                'cbu':self.cbu,    
-                
+                'destination_bank': self.destination_bank,
+                'bank': self.bank,
+                'cbu': self.cbu,
             },
         }
