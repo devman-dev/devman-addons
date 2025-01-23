6ecfadc37edca09daa632a901896df76489111d7
NelsonTontarelli <nelartz01@gmail.com>
2025-01-23 13:31:51 -0300

    [FIX] Reporte, QWEB y API

--- payment_collection/__manifest__.py
+++ payment_collection/__manifest__.py
@@ -7,7 +7,7 @@
     'website': '',
     'license': 'LGPL-3',
     'category': 'account',
-    'depends': ['base', 'contacts', 'stock', 'report_xlsx'],
+    'depends': ['base', 'contacts', 'stock', 'report_xlsx', 'survey'],
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
