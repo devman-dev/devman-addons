72eb5906f4b6db5018c18a04abfd727348046833
wsf <alejandro.sartorio@gmail.com>
2025-08-06 13:35:51 -0300

    add casino_online gerlin

---
+++ casino_online/models/__init__.py
@@ -0,0 +1,2 @@
+from . import partner_payment_method_config
+from . import res_partner
---
+++ casino_online/models/res_partner.py
@@ -0,0 +1,16 @@
+from odoo import models, fields
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    daily_deposit_limit = fields.Float(string='Límite diario de depósito')
+    weekly_deposit_limit = fields.Float(string='Límite semanal de depósito')
+    monthly_deposit_limit = fields.Float(string='Límite mensual de depósito')
+    
+    
+    account_number = fields.Char('Cuenta Bancaria')
+    bank_name = fields.Char('Banco')
+    account_type = fields.Char('Tipo de Cuenta')
+    cbu = fields.Char('CBU')
+    cuil = fields.Char('CUIL')
+    nuevo_cbu = fields.Char('Nuevo CBU')
