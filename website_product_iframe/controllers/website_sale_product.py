1e939e8e1eed9bc8b34e4126a93dda77a0c53195
Gerlin Matos <60184389+G02-Manuel@users.noreply.github.com>
2025-08-08 00:43:49 -0400

    [IMP] Add session log in the backend module

--- website_product_iframe/controllers/website_sale_product.py
+++ website_product_iframe/controllers/website_sale_product.py
@@ -1,4 +1,5 @@
 from odoo import http
+from datetime import datetime, timedelta
 from odoo.http import request
 from werkzeug.utils import redirect
 from odoo.addons.website_sale.controllers.main import WebsiteSale
@@ -13,5 +14,23 @@ class WebsiteShop(WebsiteSale):
         if not request.website.has_ecommerce_access():
             return request.redirect('/web/login')
         if product.iframe_url:
+            user = request.env.user
+
+            # Opcional: obtener balance inicial (ej. de wallet o cuenta)
+            initial_balance = 0.0  # Actualiza con lógica real si aplica
+
+            # Crear sesión
+            session = request.env['casino.game.session'].sudo().create({
+                'game_id': product.id,
+                'user_id': user.id,
+                'start_datetime': datetime.now(),
+                'end_datetime': datetime.now() + timedelta(minutes=5),
+                'initial_balance': 250,
+                'final_balance': 100,
+                'result': 'loss',
+                'currency_id': user.company_id.currency_id.id,
+                'state': 'finished',
+                'description': f'Sesión iniciada por {user.name}'
+            })
             return redirect(product.iframe_url)
         return request.render("website_sale.product", self._prepare_product_values(product, category, search, **kwargs))
