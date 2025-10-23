bfccf54e3fe74ac7c5892be25fbf81ab7806452e
pmarchionno <pmarchionno@gmail.com>
2025-10-23 00:36:15 -0300

    [UPD] Agrego permisos y boton para cerrar la tarea

--- maintenance_custom_fela/__manifest__.py
+++ maintenance_custom_fela/__manifest__.py
@@ -2,15 +2,16 @@
     "name": "Maintenance Custom Fela",
     "summary": "Extensiones de configuración para Maintenance (view_vendor 0/1, Sitios y Sectores)",
     "version": "18.0.1.0.0",
-    "author": "Devman3",
+    "author": "Tu Empresa",
     "website": "",
     "category": "Maintenance",
     "license": "LGPL-3",
     "depends": [
         "maintenance",
-        "dev_maintenance_purchase_agreement",
+         "dev_maintenance_purchase_agreement",
     ],
     "data": [
+        "security/maintenance_security.xml",
         "security/ir.model.access.csv",
         "views/maintenance_request_views.xml",
         "views/maintenance_site_views.xml",
