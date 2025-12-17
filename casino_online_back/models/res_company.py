89ea5c8b890bacbe71e7efd8e5511a8b99a24088
pmarchionno <pmarchionno@gmail.com>
2025-12-17 19:33:42 -0300

    pr

--- casino_online_back/models/res_company.py
+++ casino_online_back/models/res_company.py
@@ -69,14 +69,14 @@ class ResCompany(models.Model):
         if operation not in ('in', 'out', 'out_final', 'out_withdrawals'):
             raise UserError(_("El parámetro 'operation' debe ser 'in', 'out', 'out_final' o 'out_withdrawals'."))
 
-        if not company.casino_deposit_journal_id:
-            raise UserError(_("Configure el diario 'casino_deposit_journal_id' en la compañía."))
+        # if not company.casino_deposit_journal_id:
+        #     raise UserError(_("Configure el diario 'casino_deposit_journal_id' en la compañía."))
 
-        if not company.casino_bet_transfer_journal_id:
-            raise UserError(_("Configure el diario 'casino_bet_transfer_journal_id' en la compañía."))
+        # if not company.casino_bet_transfer_journal_id:
+        #     raise UserError(_("Configure el diario 'casino_bet_transfer_journal_id' en la compañía."))
 
-        deposit_journal = 7 #company.casino_deposit_journal_id
-        bet_transfer_journal = 6 #company.casino_bet_transfer_journal_id
+        deposit_journal = 6 #company.casino_deposit_journal_id
+        bet_transfer_journal = 7 #company.casino_bet_transfer_journal_id
 
         date = date or fields.Date.context_today(self)
         label = label or (operation == 'in' and _("Entrada de dinero") or _("Salida de dinero"))
@@ -111,7 +111,7 @@ class ResCompany(models.Model):
                 'date': date,
                 'currency_id': company.currency_id.id,
                 'journal_id': bet_transfer_journal, #.id,  # Desde diario operativo
-                'destination_journal_id': deposit_journal, #.id,  # Hacia diario de depósitos
+                # 'destination_journal_id': deposit_journal, #.id,  # Hacia diario de depósitos
                 'payment_reference': memo or label,
                 'is_reconciled': True,
                 'is_internal_transfer': True,
