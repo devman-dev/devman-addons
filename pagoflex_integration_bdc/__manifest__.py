4c19961b5a051e89559b7304b674c2d2259c188b
pmarchionno <pmarchionno@gmail.com>
2026-02-27 15:31:41 -0300

    [NEW] se agrega módulo integración con banco de comercio

---
+++ pagoflex_integration_bdc/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Pagoflex Integration DBC",
+    "summary": "Exponer URL para cierre de cuenta (Sivep/Pagoflex)",
+    "version": "1.0",
+    "author": "Hitofusion",
+    "category": "Custom",
+    "depends": ["base", "web"],
+    "data": [
+        "views/pagoflex_account_close_form.xml",
+        "views/pagoflex_account_close_website.xml"
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ pagoflex_integration_bdc/models/__init__.py
@@ -0,0 +1,1 @@
+from . import pagoflex_account_close
